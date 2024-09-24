b0f03e9aba5b1860d7407e09da02de50ba0623ca
marionogueira <helberfrd1@gmail.com>
2024-09-24 13:58:38 -0300

    Ajuste no auto scroll do titulo

--- app/src/main/java/com/example/podcastrss/ui/screens/PlayerPodcastScreen.kt
+++ app/src/main/java/com/example/podcastrss/ui/screens/PlayerPodcastScreen.kt
@@ -1,13 +1,8 @@
 package com.example.podcastrss.ui.screens
 
 import androidx.compose.animation.Crossfade
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,7 +11,6 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -28,29 +22,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaMetadata
 import androidx.media3.common.Player
@@ -79,20 +62,6 @@ fun PlayerPodcastScreen(
 
     LaunchedEffect(url,guid) {
         viewModel.initPlayer(url,guid)
-    }
-
-    val infiniteTransition = rememberInfiniteTransition(label = "")
-    val scroll by infiniteTransition.animateFloat(
-        initialValue = 1f,
-        targetValue = -1f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(9000, easing = LinearEasing),
-            repeatMode = RepeatMode.Restart
-        ), label = ""
-    )
-
-    var hasOverFlow by remember {
-        mutableStateOf(false)
     }
 
     val context = LocalContext.current
@@ -172,20 +141,9 @@ fun PlayerPodcastScreen(
                 color = Color.White,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .then(
-                        if (hasOverFlow) {
-                            Modifier
-                                .offset(x = Dp(500 * scroll))
-                        } else {
-                            Modifier
-                                .padding(start = 32.dp)
-                        }
-                    ),
+                    .basicMarquee()
+                ,
                 maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                onTextLayout = {
-                    hasOverFlow = it.hasVisualOverflow
-                },
                 fontSize = 24.sp
             )
 
